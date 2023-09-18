f608f5141ad5260953e1202cd02308616623b23f
Rudy Regazzoni <110470341+rudy-regazzoni-sonarsource@users.noreply.github.com>
2023-09-18 15:48:24 +0200

    Improved tests

--- StringCalculatorKata/src/test/java/com/example/StringCalculatorTest.java
+++ StringCalculatorKata/src/test/java/com/example/StringCalculatorTest.java
@@ -1,21 +1,29 @@
 package com.example;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringCalculatorTest {
-  @Test
-  void shouldReturnZeroForEmptyInput() {
-    String result = StringCalculator.add("");
 
-    assertEquals("0", result);
+  @ParameterizedTest
+  @MethodSource
+  void shouldReturnSingleDigit(String input, String expected) {
+    String result = StringCalculator.add(input);
+
+    assertEquals(expected, result);
   }
-  @Test
-  void shouldReturnSingleDigit() {
-    String result = StringCalculator.add("1");
 
-    assertEquals("1", result);
+  static List<Arguments> shouldReturnSingleDigit() {
+    return List.of(Arguments.of("0", "0"),
+      Arguments.of("", "0"),
+      Arguments.of("1", "1"),
+      Arguments.of("2", "2"));
   }
 
 
